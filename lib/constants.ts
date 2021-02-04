aea706f7f7d703e5b78e1ce9563fc16820f361c6
404 Social Club <admin@peyoksocialclub.com>
2021-02-04 21:40:28 +0700

    Beta Tester
    beta v1.1

--- lib/constants.ts
+++ lib/constants.ts
@@ -14,19 +14,19 @@
  * limitations under the License.
  */
 
-export const SITE_URL = 'https://demo.vercel.events';
+export const SITE_URL = 'https://peyoksocialclub.com';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
-export const TWITTER_USER_NAME = 'vercel';
-export const BRAND_NAME = 'ACME';
-export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
-export const SITE_NAME = 'ACME Conf';
+export const TWITTER_USER_NAME = 'peyoksocialclub';
+export const BRAND_NAME = '404 Social Club';
+export const SITE_NAME_MULTILINE = ['404 Event', 'Conf'];
+export const SITE_NAME = '404 Event x Peyok Social Club';
 export const META_DESCRIPTION =
   'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
 export const SITE_DESCRIPTION =
   'An interactive online experience by the community, free for everyone.';
-export const DATE = 'October 27, 2020';
-export const SHORT_DATE = 'Oct 27 - 9:00am PST';
-export const FULL_DATE = 'Oct 27th 9am Pacific Time (GMT-7)';
+export const DATE = 'October 11, 2021';
+export const SHORT_DATE = 'Oct 11 - 9:00am PST';
+export const FULL_DATE = 'Oct 11th 9am Pacific Time (GMT-7)';
 export const TWEET_TEXT = META_DESCRIPTION;
 export const COOKIE = 'user-id';
 
@@ -37,23 +37,23 @@ export const COPYRIGHT_HOLDER = process.env.NEXT_PUBLIC_COPYRIGHT_HOLDER;
 
 export const CODE_OF_CONDUCT =
   'https://www.notion.so/vercel/Code-of-Conduct-Example-7ddd8d0e9c354bb597a0faed87310a78';
-export const REPO = 'https://github.com/vercel/virtual-event-starter-kit';
+export const REPO = 'https://github.com/404SocialClub/404event';
 export const SAMPLE_TICKET_NUMBER = 1234;
 export const NAVIGATION = [
   {
-    name: 'Stage A',
+    name: 'Chapter 1',
     route: '/stage/a'
   },
   {
-    name: 'Stage C',
+    name: 'Chapter 2',
     route: '/stage/c'
   },
   {
-    name: 'Stage M',
+    name: 'Chapter 3',
     route: '/stage/m'
   },
   {
-    name: 'Stage E',
+    name: 'Chapter 4',
     route: '/stage/e'
   },
   {
@@ -65,11 +65,11 @@ export const NAVIGATION = [
     route: '/speakers'
   },
   {
-    name: 'Expo',
+    name: 'Company',
     route: '/expo'
   },
   {
-    name: 'Jobs',
+    name: 'Works',
     route: '/jobs'
   }
 ];

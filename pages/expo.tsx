e10515b072cbffd00c5d1ac768df0414f599612f
404 Social Club <admin@peyoksocialclub.com>
2021-02-05 10:00:11 +0700

    update V 1.3
    Data & Logo ala ala

--- pages/expo.tsx
+++ pages/expo.tsx
@@ -31,7 +31,7 @@ type Props = {
 
 export default function ExpoPage({ sponsors }: Props) {
   const meta = {
-    title: 'Expo - Virtual Event Starter Kit',
+    title: '404 Family',
     description: META_DESCRIPTION
   };
 
--- pages/expo/[slug].tsx
+++ pages/expo/[slug].tsx
@@ -30,7 +30,7 @@ type Props = {
 
 export default function SponsorPage({ sponsor }: Props) {
   const meta = {
-    title: 'Demo - Virtual Event Starter Kit',
+    title: '404 Event',
     description: META_DESCRIPTION
   };
 
